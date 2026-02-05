ee20b4b63d7b27f6f8da19c2318fe7fa22d43164
mimopanda1313 <mimidiemlam@gmail.com>
2026-02-05 15:11:44 -0600

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "Zuò de hǎo!! You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
