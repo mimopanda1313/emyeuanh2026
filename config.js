0666f9c0145616e3b42d94d70fea6c0a37308e49
mimopanda1313 <mimidiemlam@gmail.com>
2026-02-05 14:13:23 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,17 +5,17 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+    valentineName: "Nathan",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Valentine? 🥺​👉​👈​💝​",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        hearts: ['😘​', '💖', '💝', '🥰​', '💓','❤️‍🔥​','😍​'],  // Heart emojis
+        bears: ['🐒​','🐢​', '🐼']                       // Cute bear emojis
     },
 
     // Questions and answers
@@ -33,7 +33,7 @@ const CONFIG = {
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
+            text: "Will you be my Valentine? 🥺🌹", // The big question!
             yesBtn: "Yes!",                                             // Text for "Yes" button
             noBtn: "No"                                                 // Text for "No" button
         }
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! Thank you for being my very first Valentines!! 🎉💝💖💝💓",
+        message: "Can't wait to give you a (light) smooch and (<10sec) hug! I love you anh!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
